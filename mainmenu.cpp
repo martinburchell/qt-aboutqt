9910489efef1f8740d89bd770e59af070ed0c7e0
Martin Burchell <mb2353@cam.ac.uk>
2023-08-07 10:44:19 +0100

    Try removing the dummy layout

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -9,15 +9,10 @@
 
 MainMenu::MainMenu()
 {
-    auto dummy_layout = new QVBoxLayout();
-    dummy_layout->setContentsMargins(0, 0, 0, 0);
-    setLayout(dummy_layout);
-    auto dummy_widget = new QWidget();
-    dummy_layout->addWidget(dummy_widget);
-
     auto mainlayout = new QVBoxLayout();
     mainlayout->setContentsMargins(0, 0, 0, 0);
-    dummy_widget->setLayout(mainlayout);
+
+    setLayout(mainlayout);
 
     m_p_listwidget = new QListWidget();
     mainlayout->addWidget(m_p_listwidget);
